e134a2f3eb316b7e710f8c716c7ecc98e3e13743
GlebKolyano <glebkolyano38@gmail.com>
2023-03-27 07:55:15 +0300

    init: start weather-app project

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot({}, {}),
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
